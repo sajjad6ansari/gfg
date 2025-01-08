2ba87cdfd35905d7037a507b033a1c841837fdd4
Sajjad Ansari <132455347+sajjad6ansari@users.noreply.github.com>
2025-01-08 21:36:47 +0530

    Update Count-the-number-of-possible-triangles.cpp

--- Count-the-number-of-possible-triangles.cpp
+++ Count-the-number-of-possible-triangles.cpp
@@ -45,18 +45,21 @@ class Solution {
 
 //--------------------------------------------optimal using two pointer------------------------------------------------
 
-class Solution {
+   class Solution {
+public:
     // Function to count the number of possible triangles.
-    // code for c++ and java is same so I am providing only java code.
-    static int countTriangles(int arr[]) {
-        // code here
-        Arrays.sort(arr);
-        int ans=0;
-        for(int i=arr.length-1;i>=0;i--){
-            int st=0, end=i-1;
-            while(st<end){
-                if(arr[st]+arr[end]>arr[i]){
-                    ans+=end-st;
+     int countTriangles(vector<int>& arr) {
+        // Sort the array
+        sort(arr.begin(), arr.end());
+        int ans = 0;
+
+        // Loop to find possible triangles
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            int st = 0, end = i - 1;
+            while (st < end) {
+                // If the sum of two sides is greater than the third side
+                if (arr[st] + arr[end] > arr[i]) {
+                    ans += end - st;  // All pairs between st and end form valid triangles
                     end--;
                 }
                 else {
@@ -66,5 +69,6 @@ class Solution {
         }
         return ans;
     }
-}
+
+};
 
